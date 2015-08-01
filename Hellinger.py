cb1db3f8d1038105fe66d69cc21064782092d6cf
zweiss <zweiss@Zarahs-MBP.localdomain>
2015-08-01 13:04:28 +0200

    Hellinger distance fully implemented
    but not tested, yet

--- Hellinger.py
+++ Hellinger.py
@@ -1,9 +1,26 @@
 __author__ = 'zweiss'
 
 
+""" Calculates the Hellinger distance for two distributions
+    @param distr1 first distribution
+    @param distr2 second distribution
+    @return -1 if distributions don't have equal length and Hellinger distance else
+"""
 def hellinger(distr1, distr2):
-    return 1 / 2**.5 * (sum(euclidean(distr1, distr2)))**.5
+    return euclidean_sqrt(distr1, distr2) / 2**.5
 
 
-def euclidean(distr1, distr2):
-    return []
+""" Calculates the Euclidean distance for two distributions, but sums over the difference of the square roots
+    @param distr1 first distribution
+    @param distr2 second distribution
+    @return -1 if distributions don't have equal length and result else
+"""
+def euclidean_sqrt(distr1, distr2):
+    if len(distr1) == len(distr2):
+        tmp = []
+        for i in len(distr1):
+            tmp[i] += (distr1[i]**.5 - distr2[i]**.5)**2
+        return (sum(tmp))**.5
+    else:
+        return -1
+
--- RSA_Model.py
+++ RSA_Model.py
@@ -1,9 +1,30 @@
 __author__ = 'Valentin'
 
 from Utils import read_mat, uniform_dist_discrete
+from math import e
+from numpy import log
+from JSD import jsd
 
-def rsa():
+x = 0
+
+def rsa(x_prime):
+    global x
+    x = x_prime
+
+
+def listener0(message_index):
+    global x
     pass
+
+
+def speaker1(referent, rationality=1):
+    return e**(rationality*log(listener0(referent)))
+
+
+
+def listener2(message, salience, rationality=1):
+    pass
+
 
 if __name__ == '__main__':
 
